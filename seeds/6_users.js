70d4141b8ee31a15ed56c1e12c179cb68e9afbe1
Matt Stuhring <whereyouatmatt@gmail.com>
2018-07-04 12:52:53 -0700

    Recover from user auth error

---
+++ seeds/6_users.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.seed = function(knex) {
+  return knex('users').del()
+  .then(() => knex('users').insert([
+    {
+      user_id: 1,
+      user_email: 'test@test.com',
+      user_hashed_password: '$2a$10$e9wOPCUIDk3LT9xw.Heii.zvP9PhV0CkES.kllKLw6x7rlkNAU7Da',
+      user_access: 'admin',
+      user_reset_password_token: '',
+      created_at: new Date('2018-04-15 12:12:16 UTC'),
+      updated_at: new Date('2018-04-15 12:12:16 UTC')
+    }])
+  )
+  .then(() => knex.raw(
+      "SELECT setval('users_user_id_seq', (SELECT MAX(user_id) FROM users));"
+    )
+  );
+};
