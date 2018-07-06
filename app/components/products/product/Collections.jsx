1c18d764bd0260d81ba2d02b4e1c7114d309885d
Matt Stuhring <whereyouatmatt@gmail.com>
2018-07-06 08:54:49 -0700

    Hide & show product update btn

--- app/components/products/product/Collections.jsx
+++ app/components/products/product/Collections.jsx
@@ -4,7 +4,7 @@ import { Button, Modal, Tooltip, OverlayTrigger } from 'react-bootstrap';
 import { Link } from 'react-router';
 import Header from 'Header';
 import { Image } from 'cloudinary-react';
-
+import AuthService from 'AuthService';
 const cloudName = 'fusedglassbyceleste';
 
 
@@ -20,6 +20,8 @@ export default class Collections extends React.Component {
       price: '',
       id: null
     };
+
+    this.Auth = new AuthService();
   }
 
   componentDidMount() {
@@ -51,7 +53,7 @@ export default class Collections extends React.Component {
     }
   }
 
-  // *********************  RENDER  ****************************
+  // ***************************  RENDER  ********************************
   render() {
     const { collection } = this.props.params;
 
@@ -59,17 +61,20 @@ export default class Collections extends React.Component {
       <Tooltip id="tooltip"><strong>Edit</strong></Tooltip>
     );
 
-    return (
-      <div>
-        <Header category={this.props.params.category} collection={this.props.params.collection}/>
-
-        {this.state.products.map((p) => {
-          return <div className="col-sm-4" key={p.product_id}>
-            <div>
+    const availableProducts = () => {
+      if (this.state.products.length === 0) {
+        return (<div>
+          <h4 className="text-center"><em>No products to display!</em></h4>
+        </div>);
+      } else {
+        if (this.Auth.loggedIn()) {
+          return this.state.products.map((p) => {
+            return <div className="col-sm-4" key={p.product_id}>
               <div className="thumbnail">
                 <div className="row btn-wrap">
                   <div className="col-sm-12 text-right">
                     <OverlayTrigger placement="top" overlay={edit}>
+                      {/* LINK TO UPDATE PRODUCT FORM */}
                       <Link to={`/productupdate/${p.product_id}`}>
                         <Button bsStyle="success">
                           <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
@@ -86,13 +91,40 @@ export default class Collections extends React.Component {
                   <p>{p.product_description}</p>
                   <p>{p.product_price}</p>
                   <Link to={`/productdetails/${p.product_id}`}>
-                    <Button bsStyle="primary">Buy</Button>
+                    <Button bsStyle="primary">Buy Now</Button>
                   </Link>
                 </div>
               </div>
             </div>
-          </div>
-        })}
+          });
+        } else {
+          return this.state.products.map((p) => {
+            return <div className="col-sm-4" key={p.product_id}>
+              <div className="thumbnail img-padd">
+
+                <Image cloudName={cloudName} publicId={p.product_image_public_id} width="300" height="200" crop="pad" />
+
+                <div className="caption">
+                  <h4>{p.product_name}</h4>
+                  <p>{p.product_description}</p>
+                  <p>{p.product_price}</p>
+                  <Link to={`/productdetails/${p.product_id}`}>
+                    <Button bsStyle="primary">Buy Now</Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          });
+        }
+      }
+    }
+
+    return (
+      <div>
+        <Header category={this.props.params.category} collection={this.props.params.collection}/>
+
+        {availableProducts()}
+
       </div>
     );
   }
